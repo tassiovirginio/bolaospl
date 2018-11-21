3ad8116b265658a352bb8a2394c854322bf23007
Joselito Jr <motajoselito@hotmail.com>
2018-11-21 17:23:01 -0300

    Adiciona classe Campeonato ao Business do projeto

---
+++ src/bolao/src/main/java/br/ufba/reuse/bolao/business/CampeonatoBusiness.java
@@ -0,0 +1,38 @@
+package br.ufba.reuse.bolao.business;
+
+import br.ufba.reuse.bolao.business.daos.GrupoDAO;
+import br.ufba.reuse.bolao.entities.Campeonato;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.hibernate.criterion.Restrictions.*;
+
+@Component
+@Transactional
+public class CampeonatoBusiness {
+
+    @Autowired
+    private CampeonatoDAO dao;
+
+    public int size(){
+        return dao.size();
+    }
+
+    public void save(Campeonato candidato){
+        dao.save(candidato);
+    }
+
+    public List<Campeonato> listAll(){
+        return dao.listAll();
+    }
+
+    public Grupo findById(Long id){
+        return dao.findById(id);
+    }
+
+}
