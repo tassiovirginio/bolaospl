a0ebdd77df87482aed6e998fece950bbb3512791
Tássio Virgínio <tassiovirginino@gmail.com>
2018-11-21 16:42:47 -0300

    adicionando enteidades

---
+++ src/bolao/src/main/java/br/ufba/reuse/bolao/pages/LoginPage.java
@@ -0,0 +1,15 @@
+package br.ufba.reuse.bolao.pages;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.annotation.mount.MountPath;
+
+@MountPath("login")
+public class LoginPage extends WebPage{
+	private static final long serialVersionUID = 1L;
+
+	public LoginPage() {
+
+
+	}
+
+}
