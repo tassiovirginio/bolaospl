4c6f941802a760cd60bdae2eb69ee07386afd3ef
Tássio Virgínio <tassiovirginino@gmail.com>
2018-12-02 19:48:07 -0300

    adicionando o feedback da pagina

--- src/bolao/src/main/java/br/ufba/reuse/bolao/pages/RecuperarPage.java
+++ src/bolao/src/main/java/br/ufba/reuse/bolao/pages/RecuperarPage.java
@@ -2,7 +2,11 @@ package br.ufba.reuse.bolao.pages;
 
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.PropertyModel;
 import org.wicketstuff.annotation.mount.MountPath;
 
 import br.ufba.reuse.bolao.business.UsuarioBusiness;
@@ -13,10 +17,29 @@ public class RecuperarPage extends WebPage {
 
     @SpringBean
     private UsuarioBusiness usuarioBusiness;
+    
+    private String email;
 
     public RecuperarPage() {
     	
+    	add(new Link("linkLoginPage") {
+			@Override
+			public void onClick() {
+				setResponsePage(new LoginPage());
+			}
+		});
+    	
     	add(new FeedbackPanel("feedback"));
+    	
+    	Form form = new Form("form") {
+			protected void onSubmit() {
+				error("Feature ainda não implementada!!!");
+			}
+    	};
+    	
+    	add(form);
+    	
+    	form.add(new TextField<String>("email", new PropertyModel<String>(this, "email")));
     	
     }
 
