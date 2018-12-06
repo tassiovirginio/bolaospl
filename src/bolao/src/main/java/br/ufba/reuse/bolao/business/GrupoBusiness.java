d2956745ad8dfe597aeda39d1f02645bd0bbd1d6
Joselito Jr <motajoselito@hotmail.com>
2018-12-06 03:58:53 -0300

    Adiciona métodos de negócio de Grupo

--- src/bolao/src/main/java/br/ufba/reuse/bolao/business/GrupoBusiness.java
+++ src/bolao/src/main/java/br/ufba/reuse/bolao/business/GrupoBusiness.java
@@ -1,5 +1,8 @@
 package br.ufba.reuse.bolao.business;
 
+import static org.hibernate.criterion.Restrictions.eq;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -8,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import br.ufba.reuse.bolao.business.daos.GrupoDAO;
 import br.ufba.reuse.bolao.business.daos.util.BusinessGeneric;
 import br.ufba.reuse.bolao.entities.Grupo;
+import br.ufba.reuse.bolao.entities.Usuario;
 
 @Component
 @Transactional
@@ -20,5 +24,53 @@ public class GrupoBusiness extends BusinessGeneric<GrupoDAO, Grupo> {
 		}
         return lista;
     }
+	
+	public Grupo searchByName(String name)
+	{
+		return dao.findByCriteriaReturnUniqueResult(eq("nome", name));
+	}
+	
+	public Grupo searchByNameAndDescription(String name, String desc)
+	{
+		return dao.findByCriteriaReturnUniqueResult(eq("nome", name), eq("descricao", desc));
+	}
+	
+	public List<Grupo> findAllParticipantGroups(Usuario user)
+	{
+		List<Grupo> list = dao.listAll();
+	
+		List<Grupo> userGroups = new ArrayList<Grupo>();
+	
+		for(Grupo l:list)
+		{
+			for(Usuario usr:l.getParticipantes())
+			{
+				if(usr.equals(user))				
+					userGroups.add(l);
+			}	
+		}
+				
+		return userGroups;
+	}
+	
+	public Usuario verifyParticipants(String name, Usuario user)
+	{
+		Usuario usr = null;
+		
+		Grupo gr = new Grupo();
+		
+		gr = dao.findByCriteriaReturnUniqueResult(eq("nome", name));
+		
+		if(gr.getDono().equals(user))
+			return gr.getDono();
+		
+		for(Usuario u: gr.getParticipantes())
+		{
+			if(u.equals(user))
+				return u;
+		}
+		
+		return usr;
+	}
 	
 }
