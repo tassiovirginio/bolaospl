f57f211e00f8c7edc6265af41b03006c2828d115
Tássio Virgínio <tassiovirginino@gmail.com>
2018-12-16 16:39:10 -0300

    implementando

--- src/bolao/src/main/java/br/ufba/reuse/bolao/pages/RankinBolaoPage.java
+++ src/bolao/src/main/java/br/ufba/reuse/bolao/pages/RankinBolaoPage.java
@@ -13,10 +13,13 @@ import java.util.List;
 
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
@@ -34,6 +37,20 @@ public class RankinBolaoPage extends BasePage {
 
 	public RankinBolaoPage(Bolao bolao) {
 
+		List<Aposta> lista = apostaBusiness.listApostasDoBolaoOrderByPontos(bolao);
+
+		add(new Label("bolao", bolao.getNome()));
+
+		add(new ListView<Aposta>("listApostas",lista) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void populateItem(ListItem<Aposta> item) {
+				Aposta aposta = item.getModelObject();
+				item.add(new Label("nome", aposta.getApostador().getNome()));
+				item.add(new Label("pontos", aposta.getPontos()));
+			}
+		});
 
 	}
 
